79c915337d41a89d604252ea51032b07344ec67a
xiexudong <xie417184376@hotmail.com>
2019-09-08 17:56:09 +0800

    Update ExecutorServiceDemo.java

--- src/ExecutorServiceDemo.java
+++ src/ExecutorServiceDemo.java
@@ -1,9 +1,11 @@
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class ExecutorServiceDemo {
     private static int index=1;
     public static void main(String[] args) throws InterruptedException {
+
         System.out.println("hello world");
         System.out.println(Integer.MAX_VALUE);
         //建议通过ThreadPoolExecutor来创建线程池
@@ -16,17 +18,36 @@ public class ExecutorServiceDemo {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(5000);
-
-                    synchronized(this) {
-                        System.out.println("sleep 3000ms:" + index++);
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH：mm：ss：SSS");
-                        String str = sdf.format(System.currentTimeMillis());
-                        System.out.println("当前时间是：" + str);
-                    }
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                //System.out.println("runnable");
+                synchronized(this) {
+                    System.out.println("sleep 3000ms:" + index++);
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH：mm：ss：SSS");
+                    String str = sdf.format(System.currentTimeMillis());
+                    System.out.println("当前时间是：" + str);
+                }
+            }
+        };
+        Callable<String> callable=new Callable<>() {
+            @Override
+            public String call() throws Exception {
+                String str;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //System.out.println("runnable");
+                synchronized(this) {
+                    System.out.println("sleep 3000ms:" + index++);
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH：mm：ss：SSS");
+                    str = sdf.format(System.currentTimeMillis());
+                    System.out.println("当前时间是：" + str);
+                }
+                return str;
             }
         };
 //        executorService3.schedule(runnable,2000,TimeUnit.MILLISECONDS);
@@ -48,7 +69,31 @@ public class ExecutorServiceDemo {
         Error occurred during initialization of VM
         java.lang.OutOfMemoryError: unable to create native thread: possibly out of memory or process/resource limits reached*/
         for(int i=0;i<6;i++){
-            executorService.execute(runnable);
+           //executorService.execute(runnable);
+            Future future2=executorService.submit(runnable);
+            try {
+                System.out.println("future2.get():"+future2.get());
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            /*Future<String> future=executorService.submit(callable);
+            try {
+                String callableResult=future.get();
+
+                System.out.println("callableResult:"+callableResult+",future.isCancelled():"+future.isCancelled()+",future.isDone():"+future.isDone());
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            System.out.println("executorService.submit(callable):"+executorService.submit(callable));*/
+
+            /*Future<String> future= (Future<String>) executorService.submit(runnable);
+            future.cancel(true);
+            System.out.println("future.isCancelled():"+future.isCancelled()+",future.isDone():"+future.isDone());
+            try {
+                System.out.println("future.get():"+future.get());//当task执行成功的时候future.get()返回null
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }*/
         }
         executorService.shutdown();
 //        boolean isTermination=executorService3.awaitTermination(20, TimeUnit.SECONDS);
@@ -59,5 +104,7 @@ public class ExecutorServiceDemo {
         //executorService3.submit(runnable);
 //        System.out.println("isShutdown:"+executorService3.isShutdown());
 //        System.out.println("isTerminated:"+executorService3.isTerminated());
+
     }
+
 }
